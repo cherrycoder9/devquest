bca1ec440c02a10b50b9e706daab6d66b80d9a1e
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-01 06:03:21 +0900

    DTO 어노테이션 수정

--- src/main/java/devquest/model/dto/UserDto.java
+++ src/main/java/devquest/model/dto/UserDto.java
@@ -2,14 +2,16 @@
 
 package devquest.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data // Getter, Setter, ToString, RequiredArgsConstructor(final 필드나 @NonNull이 붙은 필드에 대한 생성자 생성), EqualsAndHashCode
-@NoArgsConstructor // 인자가 없는 기본 생성자 생성
-@AllArgsConstructor // 모든 필드에 대한 인자를 받는 생성자 생성
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder // 빌더 패턴 적용 가능하게 함
 public class UserDto {
+    private int a;
+    private int b;
 }
