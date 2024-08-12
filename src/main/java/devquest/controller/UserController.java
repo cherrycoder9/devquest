8888dbf7b01419d5336610e2922e4d6a13d00da8
cherrycoder9 <cherrycoder9@gmail.com>
2024-08-12 12:20:05 +0900

    제목은 대문자로 시작하고 마침표는 생략
    - 어떤 변경이 이루어졌는지 설명
    - 변경한 부분에 대한 상세 설명
    - 관련된 이슈 번호나 참고 자료

--- src/main/java/devquest/controller/UserController.java
+++ src/main/java/devquest/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Getter
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
 
